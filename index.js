72f241f81a45971d76235bbbaf7c72b730ddd15a
Md Shahadat Hossain <91413520+Developer-Shahadat@users.noreply.github.com>
2024-01-13 22:05:28 +0600

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f3c1ntb.mongodb.net/?retryWrites=true&w=majority`;
-  console.log(uri);
+ 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
