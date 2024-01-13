1b4a115218b8a9155c63a644d7605ea322c0a5bd
Developer-Shahadat <shahadatbci95@gmail.com>
2024-01-13 22:00:34 +0600

    update db

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -10,7 +11,8 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
-  "mongodb+srv://shahadatbci95:0ZWPzkd1ASE5fV1K@cluster0.f3c1ntb.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f3c1ntb.mongodb.net/?retryWrites=true&w=majority`;
+  console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
